e53a8ad9fb595743e646b57693113e925dc04a58
shkaf4uk <shafarenkomaksym@gmail.com>
2021-01-09 19:13:48 +0200

    Updates

---
+++ precache-manifest.e6d21b2e95fd714879cfa87a5ee03a60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a5d7907cea3c2f5c0270ad13d2d5439",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "dbfcb5117fc154fa3485",
+    "url": "/my-app/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "3bd9ef49534d41b53790",
+    "url": "/my-app/static/css/main.8d692ba0.chunk.css"
+  },
+  {
+    "revision": "dbfcb5117fc154fa3485",
+    "url": "/my-app/static/js/2.fa401390.chunk.js"
+  },
+  {
+    "revision": "c634703c58b4d73a8943d23b8c4fb8e8",
+    "url": "/my-app/static/js/2.fa401390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bd9ef49534d41b53790",
+    "url": "/my-app/static/js/main.d01a36b9.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
